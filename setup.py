84ce2654b5d781e32339f5c70c8482084b19e975
David Greisen <dgreisen@gmail.com>
2014-05-04 13:37:46 -0400

    vbump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='UniversalClient',
-    version='0.6.1',
+    version='0.6.2',
     author='David Greisen',
     author_email='dgreisen@gmail.com',
     packages=['universalclient'],
